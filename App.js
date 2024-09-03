e5ad2ebddc437b7d0389da440f0992f5938c253d
Amit Jamshetty <126220607+AmitJamshetty@users.noreply.github.com>
2024-09-03 12:38:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-3xl font-bold underline text-green-800'>
+      Namste React
+    </div>
+  );
+}
+
+export default App;
